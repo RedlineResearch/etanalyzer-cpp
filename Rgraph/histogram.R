e27ee191a47cb84581501c6e6a85297626eb1929
Raoul Veroy <raoul@littletechtips.com>
2016-01-12 01:35:25 -0500

    Scripts for rendering the type graphs.

--- Rgraph/histogram.R
+++ Rgraph/histogram.R
@@ -6,50 +6,50 @@ library(ggplot2)
 cargs <- commandArgs(TRUE)
 datafile <- cargs[1]
 outfile <- cargs[2]
-width <- as.integer(cargs[3])
-height <- as.integer(cargs[4])
-title <- cargs[5]
-xlabel <- cargs[6]
+bmark <- cargs[3]
+title <- cargs[4]
+xlabel <- cargs[5]
+# width <- as.integer(cargs[4])
+# height <- as.integer(cargs[5])
+
 xcsv <- read.table( datafile, sep = ",", header = TRUE )
 
-bmark <- unique(xcsv$benchmark)
-for (b in bmark) {
-    print(paste("Processing", b))
-    flush.console()
-    subset <- xcsv[ xcsv$benchmark == b, ]
-    #--------------------------------------------------
-    # Linear scale
-    print("    - linear")
-    flush.console()
-    output <- paste0(b, "-", outfile)
-    result = tryCatch( {
-            p <- ggplot( subset, aes(x = total)) + geom_histogram( binwidth = 2 )
-            ggsave(filename = output, plot = p)
-        }, warning = function(w) {
-            print(paste("WARNING: failed on", b))
-        }, error = function(e) {
-            print(paste("ERROR: failed on", b))
-        }, finally = {
-        }
-    )
-    #--------------------------------------------------
-    # Log scale
-    print("    - log")
-    flush.console()
-    output <- paste0(b, "-log10-", outfile)
-    result = tryCatch( {
-            p <- ggplot( subset, aes(x = total)) + geom_density() # + scale_y_log10()
-            ggsave(filename = output, plot = p)
-        }, warning = function(w) {
-            print(paste("WARNING: failed on", b))
-        }, error = function(e) {
-            print(paste("ERROR: failed on", b))
-        }, finally = {
-        }
-    )
-    print("    DONE.")
-    flush.console()
-}
+print(paste("Processing", bmark))
+flush.console()
+#--------------------------------------------------
+# Types - linear scale
+print("    - linear")
+flush.console()
+output <- paste0(bmark, "-", outfile)
+xcsv$num_types <- factor(xcsv$num_types)
+result = tryCatch( {
+        p <- ggplot( xcsv, aes(x = num_types)) + geom_bar( stat = "bin" )
+        ggsave(filename = output, plot = p)
+    }, warning = function(w) {
+        print(paste("WARNING: failed on", bmark))
+    }, error = function(e) {
+        print(paste("ERROR: failed on", bmark))
+    }, finally = {
+    }
+)
+#--------------------------------------------------
+# Types - log scale
+print("    - log")
+flush.console()
+output <- paste0(bmark, "-log10-", outfile)
+result = tryCatch( {
+        p <- ggplot( xcsv, aes(x = num_types)) + geom_bar( stat = "bin" ) + scale_y_log10()
+        ggsave(filename = output, plot = p)
+    }, warning = function(w) {
+        print(paste("WARNING: failed on", bmark))
+    }, error = function(e) {
+        print(paste("ERROR: failed on", bmark))
+    }, finally = {
+    }
+)
+print("    DONE.")
+flush.console()
+
 # png(filename = outfile, width = width, height = height)
 # qplot( byNodes, data = xcsv, xlab = xlabel, main = title, geom = "histogram", binwidth = 1 )
 # dev.off()
---
+++ run_histogram.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+import re
+import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument( "directory", help = "Target directory with CSV files." )
+
+args = parser.parse_args()
+
+def render_histogram( csvfile = None,
+                      bmark = None,
+                      title = None,
+                      xlabel = None ):
+    outpdf = "types.pdf"
+    cmd = [ "/data/rveroy/bin/Rscript",
+            "./histogram.R",
+            csvfile, outpdf,
+            bmark, title, xlabel ]
+    print "Running histogram.R on %s -> %s" % (csvfile, outpdf)
+    rproc = subprocess.Popen( cmd,
+                              stdout = subprocess.PIPE,
+                              stdin = subprocess.PIPE,
+                              stderr = subprocess.PIPE )
+    result = rproc.communicate()
+    return result
+
+csvre = re.compile( "([a-z0-9_]+)-basic_cycle_analyze-.*\.csv" )
+for item in os.listdir( args.directory ):
+    m = csvre.match(item)
+    if m:
+        bmark = m.group(1)
+        title = "%s types" % bmark
+        print "%s: %s" % (item, bmark)
+        result = render_histogram( csvfile = item,
+                                   bmark = bmark,
+                                   title = title,
+                                   xlabel = "type counts" )
