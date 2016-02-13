23a1b7a06df8044fc54b1e67c27cf1d4f98413ff
Raoul Veroy <raoul@littletechtips.com>
2016-02-13 18:36:12 -0500

    fix tables caption in R script.

--- Rgraph/tables.R
+++ Rgraph/tables.R
@@ -47,7 +47,7 @@ t1.2$died_by_stack_size_perc <- formatC( t1$died_by_stack_size_perc, digits = 2,
 
 t1.2
 # Caption for the table
-cap1 <- c("How objects died for the Dacapo and SpecJVM98 benchmarks. Benchmarks are ordered according to maximum live size. Total objects is the number of objects for the whole execution of the bencmark. Died by heap is the number of objects that died by heap action, while the percentage is over the total number of objects. Died by stack is the number of objects that died by stack action, and the percentage is also over the total number of objects. Sizes are in MB.",
+cap1 <- c("How objects died for the Dacapo and SpecJVM98 benchmarks. Benchmarks are ordered according to maximum live size. Total objects is the number of objects for the whole execution of the bencmark. Died by heap is the number of objects that died by heap action, while the percentage is over the total number of objects. Died by stack is the number of objects that died by stack action, and the percentage is also over the total number of objects. Sizes are in MB. Percentages are rounded off to two decimal digits.",
           "How objects died in the Dacapo and SpecJVM98 benchmarks.")
 
 # Latex label
