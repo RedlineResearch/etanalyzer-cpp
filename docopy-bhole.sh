4f3b51916986db87ffa585f1a737f01e89952c81
Raoul Veroy <raoul@littletechtips.com>
2016-02-28 18:42:58 -0500

    Updating the copy script for black-hole.

--- docopy-bhole.sh
+++ docopy-bhole.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cp -vf _201_COMPRESS/_201_compress-DGROUPS.csv _205_RAYTRACE/_205_raytrace-DGROUPS.csv \
+    _209_DB/_209_db-DGROUPS.csv \
     AVRORA/avrora-DGROUPS.csv LUINDEX/luindex-DGROUPS.csv \
+    SPECJBB/specjbb-DGROUPS.csv \
     XALAN/xalan-DGROUPS.csv  \
     /h/rveroy/src/data-ismm-2016/Death-groups
