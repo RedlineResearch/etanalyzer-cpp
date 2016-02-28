a52d54ee33af1c552c0a9f878e5ac61eca4e2bd8
Raoul Veroy <raoul@littletechtips.com>
2016-02-28 15:13:47 -0500

    Script for copying over the home directory that's backed up.

---
+++ docopy-bhole.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp -vf _201_COMPRESS/_201_compress-DGROUPS.csv _205_RAYTRACE/_205_raytrace-DGROUPS.csv \
+    AVRORA/avrora-DGROUPS.csv LUINDEX/luindex-DGROUPS.csv \
+    XALAN/xalan-DGROUPS.csv  \
+    /h/rveroy/src/data-ismm-2016/Death-groups
