d80d07e33d89d1f02ac2ede66d0e92dd9294eecb
Raoul Veroy <raoul@littletechtips.com>
2017-04-30 23:28:10 -0400

    Remove code from previous simulator related to heap. Add a simple map from object Id to size only.

--- 0-simulator-GC/simulator-PAGC-ver1.cpp
+++ 0-simulator-GC/simulator-PAGC-ver1.cpp
@@ -32,16 +32,15 @@ typedef std::map< string, std::vector< Summary * > > GroupSum_t;
 typedef std::map< string, Summary * > TypeTotalSum_t;
 typedef std::map< unsigned int, Summary * > SizeSum_t;
 
+typedef std::map< unsigned int, unsigned int > ObjectMap_t;
+// Map from object ID to object size
 // ----------------------------------------------------------------------
 //   Globals
 
-// -- Key object map to set of objects
-KeySet_t keyset;
-// -- Object to key object map
-ObjectPtrMap_t whereis;
+// -- The pseudo-heap
+ObjectMap_t objmap;
 
-// -- The heap
-HeapState Heap( whereis, keyset );
+// TODO: DELETE HeapState Heap( whereis, keyset );
 
 // -- Execution state
 #ifdef ENABLE_TYPE1
@@ -112,8 +111,8 @@ unsigned int read_trace_file( FILE *f,
     unsigned int field_id;
     unsigned int thread_id;
     unsigned int exception_id;
-    Object *obj;
-    Object *target;
+    // Object *obj;
+    // Object *target;
     Method *method;
     unsigned int total_garbage = 0;
 
@@ -143,16 +142,19 @@ unsigned int read_trace_file( FILE *f,
                     unsigned int els  = (tokenizer.numTokens() == 6) ? 0
                                                                      : tokenizer.getInt(5);
                     AllocSite* as = ClassInfo::TheAllocSites[tokenizer.getInt(4)];
-                    obj = Heap.allocate( tokenizer.getInt(1),
-                                         tokenizer.getInt(2),
-                                         tokenizer.getChar(0),
-                                         tokenizer.getString(3),
-                                         as,
-                                         els,
-                                         thread,
-                                         Exec.NowUp() );
+                    unsigned int my_id = tokenizer.getInt(1);
+                    unsigned int my_size = tokenizer.getInt(2);
+                    objmap[my_id] = my_size;
+                    // obj = Heap.allocate( tokenizer.getInt(1),
+                    //                      tokenizer.getInt(2),
+                    //                      tokenizer.getChar(0),
+                    //                      tokenizer.getString(3),
+                    //                      as,
+                    //                      els,
+                    //                      thread,
+                    //                      Exec.NowUp() );
                     unsigned int old_alloc_time = AllocationTime;
-                    AllocationTime += obj->getSize();
+                    AllocationTime += my_size;
                     dataout << "A " << AllocationTime << endl;
                 }
                 break;
@@ -162,47 +164,9 @@ unsigned int read_trace_file( FILE *f,
                     // U <old-target> <object> <new-target> <field> <thread>
                     // 0      1          2         3           4        5
                     // -- Look up objects and perform update
-                    unsigned int objId = tokenizer.getInt(2);
-                    unsigned int tgtId = tokenizer.getInt(3);
-                    unsigned int oldTgtId = tokenizer.getInt(1);
-                    if ((objId > 0) && (tgtId > 0)) {
-                        Heap.make_edge2( objId, tgtId );
-                    }
-                    if ((objId > 0) && (oldTgtId > 0)) {
-                        Heap.remove_edge2( objId, oldTgtId );
-                    }
-                    // TODO ============================================================ TODO
-                    // TODO unsigned int threadId = tokenizer.getInt(5);
-                    // TODO: Not sure if we need this code
-                    // TODO Thread *thread = Exec.getThread(threadId);
-                    // TODO Object *oldObj = Heap.getObject(oldTgtId);
-                    // TODO obj = Heap.getObject(objId);
-                    // TODO target = ((tgtId > 0) ? Heap.getObject(tgtId) : NULL);
-                    // TODO if (obj && target) {
-                        // TODO unsigned int field_id = tokenizer.getInt(4);
-                        // TODO Edge* new_edge = Heap.make_edge( obj, field_id,
-                        // TODO                                  target, Exec.NowUp() );
-                        // TODO if (thread) {
-                        // TODO     Method *topMethod = thread->TopMethod();
-                        // TODO     if (topMethod) {
-                        // TODO         topMethod->getName();
-                        // TODO     }
-                        // TODO     obj->updateField( new_edge,
-                        // TODO                       field_id,
-                        // TODO                       Exec.NowUp(),
-                        // TODO                       topMethod, // for death site info
-                        // TODO                       HEAP, // reason
-                        // TODO                       NULL ); // death root 0 because may not be a root
-                        // TODO     // NOTE: topMethod COULD be NULL here.
-                        // TODO }
-                        // DEBUG ONLY IF NEEDED
-                        // Example:
-                        // if ( (objId == tgtId) && (objId == 166454) ) {
-                        // if ( (objId == 166454) ) {
-                        //     tokenizer.debugCurrent();
-                        // }
-                    // TODO }
-                    // TODO ============================================================ TODO
+                    // TODO: unsigned int objId = tokenizer.getInt(2);
+                    // TODO: unsigned int tgtId = tokenizer.getInt(3);
+                    // TODO: unsigned int oldTgtId = tokenizer.getInt(1);
                 }
                 break;
 
@@ -211,16 +175,9 @@ unsigned int read_trace_file( FILE *f,
                     // D <object> <thread-id>
                     // 0    1
                     unsigned int objId = tokenizer.getInt(1);
-                    obj = Heap.getObject(objId);
-                    if (obj) {
-                        unsigned int threadId = tokenizer.getInt(2);
-                        Thread *thread = Exec.getThread(threadId);
-                        Heap.makeDead(obj, Exec.NowUp());
-                        total_garbage += obj->getSize();
-                        dataout << "G " << total_garbage << endl;
-                    } else {
-                        assert(false);
-                    }
+                    unsigned int my_size = objmap[objId];
+                    total_garbage += my_size;
+                    dataout << "G " << total_garbage << endl;
                 }
                 break;
 
@@ -264,19 +221,8 @@ unsigned int read_trace_file( FILE *f,
                 // R <objid> <threadid>
                 // 0    1        2
                 {
-                    unsigned int objId = tokenizer.getInt(1);
-                    Object *object = Heap.getObject(objId);
-                    unsigned int threadId = tokenizer.getInt(2);
-                    // cout << "objId: " << objId << "     threadId: " << threadId << endl;
-                    if (object) {
-                        object->setRootFlag(Exec.NowUp());
-                        Thread *thread = Exec.getThread(threadId);
-                        if (thread) {
-                            thread->objectRoot(object);
-                        }
-                    }
-                    root_set.insert(objId);
-                    // TODO last_map.setLast( threadId, LastEvent::ROOT, object );
+                    // TODO: unsigned int objId = tokenizer.getInt(1);
+                    // TODO: Do we need to do something here?
                 }
                 break;
 
@@ -301,132 +247,25 @@ void debug_GC_history( deque< GCRecord_t > &GC_history )
 
 int main(int argc, char* argv[])
 {
-    if (argc != 5) {
+    if (argc != 3) {
         cout << "simulator-PAGC-ver1" << endl
-             << "Usage: " << argv[0] << " <namesfile> <PAGC-config-file> <output base name> <memsize>" << endl
+             << "Usage: " << argv[0] << " <namesfile>  <output base name>" << endl
              << "      git version: " <<  build_git_sha << endl
              << "      build date : " <<  build_git_time << endl;
         exit(1);
     }
-    string dgroups_csvfile(argv[2]);
-    string basename(argv[3]);
+    // TODO string dgroups_csvfile(argv[2]);
+    string basename(argv[1]);
 
-    unsigned long memsize = std::stoul(argv[4]);
-    cout << "Memory size: " << memsize << " bytes." << endl;
-    
-    Heap.initialize_memory_deferred_VER2( memsize, // in bytes
-                                          dgroups_csvfile, // output of dgroups2db.py
-                                          3 ); // Number of groups to use
-
-    // Hard coded number at this point. TODO
     cout << "Read names file..." << endl;
     ClassInfo::read_names_file_no_mainfunc( argv[1] );
 
-    cout << "Start trace..." << endl;
+    cout << "Start running PAGC simulator on trace..." << endl;
     FILE* f = fdopen(0, "r");
     string newtrace_filename( basename + "-PAGC-TRACE.csv" );
     ofstream newtrace_file( newtrace_filename );
     unsigned int total_garbage = read_trace_file( f, newtrace_file );
     unsigned int final_time = Exec.NowUp();
-    cout << "Done at time " << Exec.NowUp() << endl
-         << "Total allocated in bytes:     " << Heap.get_total_alloc() << endl
-         << "Number of collections: " << Heap.get_number_of_collections() << endl;
-    cout << "Mark total   : " << Heap.get_mark_total() << endl
-         << "- mark saved : " << Heap.get_mark_saved() << endl
-         << "- total alloc: " << Heap.get_total_alloc() << endl;
-    // TODO << " - number of edges removed: " << Heap.get_number_edges_removed() << endl
-    // TODO << " - number of edges removal attempts: " << Heap.get_number_attempts_edges_removed() << endl
-    // TODO << " - number of region edges: " << Heap.get_region_edges_count() << endl
-    // TODO << " - number of in edges: " << Heap.get_in_edges_count() << endl
-    // TODO << " - number of out edges: " << Heap.get_out_edges_count() << endl
-    // TODO << " - number of nonregion edges: " << Heap.get_nonregion_edges_count() << endl;
-    return Heap.get_number_of_collections();
+    cout << "Done at time " << Exec.NowUp() << endl;
+    return 0;
 }
-
-//--------------------------------------------------------------------------------
-// COMMENTED OUT CODE
-//--------------------------------------------------------------------------------
-/*
-
-void sanity_check()
-{
-   if (Now > obj->getDeathTime() && obj->getRefCount() != 0) {
-   nonzero_ref++;
-   printf(" Non-zero-ref-dead %X of type %s\n", obj->getId(), obj->getType().c_str());
-   }
-}
-
-bool member( Object* obj, const ObjectSet& theset )
-{
-    return theset.find(obj) != theset.end();
-}
-
-bool member( Object* obj, const ObjectSet& theset )
-{
-    return theset.find(obj) != theset.end();
-}
-
-unsigned int closure( ObjectSet& roots,
-                      ObjectSet& premarked,
-                      ObjectSet& result )
-{
-    unsigned int mark_work = 0;
-
-    vector<Object*> worklist;
-
-    // -- Initialize the worklist with only unmarked roots
-    for ( ObjectSet::iterator i = roots.begin();
-          i != roots.end();
-          i++ ) {
-        Object* root = *i;
-        if ( !member(root, premarked) ) {
-            worklist.push_back(root);
-        }
-    }
-
-    // -- Do DFS until the worklist is empty
-    while (worklist.size() > 0) {
-        Object* obj = worklist.back();
-        worklist.pop_back();
-        result.insert(obj);
-        mark_work++;
-
-        const EdgeMap& fields = obj->getFields();
-        for ( EdgeMap::const_iterator p = fields.begin();
-              p != fields.end();
-              p++ ) {
-            Edge* edge = p->second;
-            Object* target = edge->getTarget();
-            if (target) {
-                // if (target->isLive(Exec.NowUp())) {
-                if ( !member(target, premarked) &&
-                     !member(target, result) ) {
-                    worklist.push_back(target);
-                }
-                // } else {
-                // cout << "WEIRD: Found a dead object " << target->info() << " from " << obj->info() << endl;
-                // }
-            }
-        }
-    }
-
-    return mark_work;
-}
-
-unsigned int count_live( ObjectSet & objects, unsigned int at_time )
-{
-    int count = 0;
-    // -- How many are actually live
-    for ( ObjectSet::iterator p = objects.begin();
-          p != objects.end();
-          p++ ) {
-        Object* obj = *p;
-        if (obj->isLive(at_time)) {
-            count++;
-        }
-    }
-
-    return count;
-}
-
-*/
